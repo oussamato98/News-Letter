969f096799c5786395e946227ba987f37c9bae50
oussamato98 <oussamaaouad98@gmail.com>
2023-05-15 09:06:30 +0100

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,66 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
+/* const key = "d08751a996d585e2f1994fd4f15eaf3d-us21";
+    id = ddae1ebd95
+ */
 app.listen(3000,function(){
     console.log("Server is runnig on 3000");
+});
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/signup.html");
+
+});
+
+app.post("/",function(req,res){
+
+    var firstName =req.body.prenom;
+    var lastName=req.body.nom;
+    var email = req.body.email;
+
+    const data = {
+        members : [
+            {
+                email_address : email ,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/ddae1ebd95";
+    
+    const options = {
+
+        method: "POST",
+        auth : "anystring:d08751a996d585e2f1994fd4f15eaf3d-us21"
+
+      };
+
+
+    const request = https.request(url , options , function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        });
+
+    });
+
+    request.write(jsonData);
+    request.end();
+
+
 });
